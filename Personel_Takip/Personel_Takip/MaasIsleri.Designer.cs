cfb69ae1f4d1af765c47caf060096e4221d5b64a
moztrk <117629976+moztrk@users.noreply.github.com>
2024-01-02 20:59:38 +0300

    12

--- Personel_Takip/Personel_Takip/MaasIsleri.Designer.cs
+++ Personel_Takip/Personel_Takip/MaasIsleri.Designer.cs
@@ -35,15 +35,15 @@
             this.labelNormalMaas = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.labelNormalPersonel = new System.Windows.Forms.Label();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBoxNormal = new System.Windows.Forms.ComboBox();
             this.radioButtonYuzde = new System.Windows.Forms.RadioButton();
             this.radioButtonNormal = new System.Windows.Forms.RadioButton();
             this.panelYuzde = new System.Windows.Forms.Panel();
             this.labelYuzde = new System.Windows.Forms.Label();
             this.labelYuzdePersonel = new System.Windows.Forms.Label();
             this.btnMaasGuncelleYuzde = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.txtYuzdeMaas = new System.Windows.Forms.TextBox();
+            this.comboBoxYuzde = new System.Windows.Forms.ComboBox();
             this.panelNormal.SuspendLayout();
             this.panelYuzde.SuspendLayout();
             this.SuspendLayout();
@@ -56,7 +56,7 @@
             this.panelNormal.Controls.Add(this.labelNormalMaas);
             this.panelNormal.Controls.Add(this.textBox2);
             this.panelNormal.Controls.Add(this.labelNormalPersonel);
-            this.panelNormal.Controls.Add(this.comboBox2);
+            this.panelNormal.Controls.Add(this.comboBoxNormal);
             this.panelNormal.Location = new System.Drawing.Point(576, 45);
             this.panelNormal.Name = "panelNormal";
             this.panelNormal.Size = new System.Drawing.Size(454, 396);
@@ -96,7 +96,7 @@
             // 
             this.textBox2.Location = new System.Drawing.Point(289, 154);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(121, 22);
+            this.textBox2.Size = new System.Drawing.Size(150, 22);
             this.textBox2.TabIndex = 2;
             // 
             // labelNormalPersonel
@@ -108,15 +108,14 @@
             this.labelNormalPersonel.Size = new System.Drawing.Size(227, 38);
             this.labelNormalPersonel.TabIndex = 1;
             this.labelNormalPersonel.Text = "Personeli Seçiniz:";
-            this.labelNormalPersonel.Click += new System.EventHandler(this.labelNormalPersonel_Click);
             // 
-            // comboBox2
+            // comboBoxNormal
             // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(289, 84);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(121, 24);
-            this.comboBox2.TabIndex = 0;
+            this.comboBoxNormal.FormattingEnabled = true;
+            this.comboBoxNormal.Location = new System.Drawing.Point(289, 84);
+            this.comboBoxNormal.Name = "comboBoxNormal";
+            this.comboBoxNormal.Size = new System.Drawing.Size(150, 24);
+            this.comboBoxNormal.TabIndex = 0;
             // 
             // radioButtonYuzde
             // 
@@ -129,7 +128,7 @@
             this.radioButtonYuzde.TabStop = true;
             this.radioButtonYuzde.Text = "Yüzde  İle Maaş Güncelle";
             this.radioButtonYuzde.UseVisualStyleBackColor = true;
-            this.radioButtonYuzde.CheckedChanged += new System.EventHandler(this.radioButtonYuzde_CheckedChanged_1);
+            this.radioButtonYuzde.CheckedChanged += new System.EventHandler(this.radioButtonYuzde_CheckedChanged);
             // 
             // radioButtonNormal
             // 
@@ -142,7 +141,7 @@
             this.radioButtonNormal.TabStop = true;
             this.radioButtonNormal.Text = "Normal Maaş Güncelle";
             this.radioButtonNormal.UseVisualStyleBackColor = true;
-            this.radioButtonNormal.CheckedChanged += new System.EventHandler(this.radioButtonNormal_CheckedChanged_1);
+            this.radioButtonNormal.CheckedChanged += new System.EventHandler(this.radioButtonNormal_CheckedChanged);
             // 
             // panelYuzde
             // 
@@ -150,8 +149,8 @@
             this.panelYuzde.Controls.Add(this.labelYuzde);
             this.panelYuzde.Controls.Add(this.labelYuzdePersonel);
             this.panelYuzde.Controls.Add(this.btnMaasGuncelleYuzde);
-            this.panelYuzde.Controls.Add(this.textBox1);
-            this.panelYuzde.Controls.Add(this.comboBox1);
+            this.panelYuzde.Controls.Add(this.txtYuzdeMaas);
+            this.panelYuzde.Controls.Add(this.comboBoxYuzde);
             this.panelYuzde.Location = new System.Drawing.Point(44, 45);
             this.panelYuzde.Name = "panelYuzde";
             this.panelYuzde.Size = new System.Drawing.Size(461, 396);
@@ -188,20 +187,20 @@
             this.btnMaasGuncelleYuzde.Text = "Güncelle";
             this.btnMaasGuncelleYuzde.UseVisualStyleBackColor = true;
             // 
-            // textBox1
+            // txtYuzdeMaas
             // 
-            this.textBox1.Location = new System.Drawing.Point(281, 143);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(121, 22);
-            this.textBox1.TabIndex = 1;
+            this.txtYuzdeMaas.Location = new System.Drawing.Point(281, 143);
+            this.txtYuzdeMaas.Name = "txtYuzdeMaas";
+            this.txtYuzdeMaas.Size = new System.Drawing.Size(159, 22);
+            this.txtYuzdeMaas.TabIndex = 1;
             // 
-            // comboBox1
+            // comboBoxYuzde
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(281, 72);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 24);
-            this.comboBox1.TabIndex = 0;
+            this.comboBoxYuzde.FormattingEnabled = true;
+            this.comboBoxYuzde.Location = new System.Drawing.Point(281, 72);
+            this.comboBoxYuzde.Name = "comboBoxYuzde";
+            this.comboBoxYuzde.Size = new System.Drawing.Size(159, 24);
+            this.comboBoxYuzde.TabIndex = 0;
             // 
             // MaasIsleri
             // 
@@ -218,7 +217,6 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MaasIsleri";
             this.Text = "Maaş Güncelleme";
-            this.Load += new System.EventHandler(this.MaasIsleri_Load);
             this.panelNormal.ResumeLayout(false);
             this.panelNormal.PerformLayout();
             this.panelYuzde.ResumeLayout(false);
@@ -237,13 +235,13 @@
         private System.Windows.Forms.Label labelYuzde;
         private System.Windows.Forms.Label labelYuzdePersonel;
         private System.Windows.Forms.Button btnMaasGuncelleYuzde;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox txtYuzdeMaas;
+        private System.Windows.Forms.ComboBox comboBoxYuzde;
         private System.Windows.Forms.Button btnNormalCıkar;
         private System.Windows.Forms.Button btnNormalEkle;
         private System.Windows.Forms.Label labelNormalMaas;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label labelNormalPersonel;
-        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBoxNormal;
     }
 }
--- Personel_Takip/Personel_Takip/MaasIsleri.cs
+++ Personel_Takip/Personel_Takip/MaasIsleri.cs
@@ -2,37 +2,107 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
 
 namespace Personel_Takip
 {
     public partial class MaasIsleri : Form
     {
+
+
         public MaasIsleri()
         {
             InitializeComponent();
+
             panelYuzde.Visible = false;
             panelNormal.Visible = false;
+            ComboBoxYuzdeDoldur();
+            ComboBoxNormalDoldur();
         }
-      
-     
+
+
         private void MaasIsleri_Load(object sender, EventArgs e)
         {
 
         }
 
-        private void labelNormalPersonel_Click(object sender, EventArgs e)
-        {
+     
 
+
+       
+        private void ComboBoxYuzdeDoldur()
+        {
+            using (OleDbConnection conn = new OleDbConnection("Provider=Microsoft.ACE.oledb.12.0;Data Source=GirisEkranı.accdb"))
+            {
+                using (OleDbCommand cmd = new OleDbCommand())
+                {
+                    try
+                    {
+                        conn.Open();
+                        cmd.Connection = conn;
+                        string query = "SELECT p_ad, p_soyad FROM personel";
+
+                        using (OleDbCommand command = new OleDbCommand(query, conn))
+                        {
+                            using (OleDbDataReader reader = command.ExecuteReader())
+                            {
+                                while (reader.Read())
+                                {
+                                    string ad = reader["p_ad"].ToString();
+                                    string soyad = reader["p_soyad"].ToString();
+                                    comboBoxYuzde.Items.Add($"{ad} {soyad}");
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Hata Oluştu: " + ex.Message);
+                    }
+                }
+            }
+        }
+        private void ComboBoxNormalDoldur()
+        {
+            using (OleDbConnection conn = new OleDbConnection("Provider=Microsoft.ACE.oledb.12.0;Data Source=GirisEkranı.accdb"))
+            {
+                using (OleDbCommand cmd = new OleDbCommand())
+                {
+                    try
+                    {
+                        conn.Open();
+                        cmd.Connection = conn;
+                        string query = "SELECT p_ad, p_soyad FROM personel";
+
+                        using (OleDbCommand command = new OleDbCommand(query, conn))
+                        {
+                            using (OleDbDataReader reader = command.ExecuteReader())
+                            {
+                                while (reader.Read())
+                                {
+                                    string ad = reader["p_ad"].ToString();
+                                    string soyad = reader["p_soyad"].ToString();
+                                    comboBoxNormal.Items.Add($"{ad} {soyad}");
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Hata Oluştu: " + ex.Message);
+                    }
+                }
+            }
         }
 
-        private void radioButtonYuzde_CheckedChanged_1(object sender, EventArgs e)// radiobuton seçme
+        private void radioButtonYuzde_CheckedChanged(object sender, EventArgs e)
         {
-
             if (radioButtonYuzde.Checked)
             {
                 panelYuzde.Visible = true;
@@ -46,7 +116,7 @@ namespace Personel_Takip
             }
         }
 
-        private void radioButtonNormal_CheckedChanged_1(object sender, EventArgs e)
+        private void radioButtonNormal_CheckedChanged(object sender, EventArgs e)
         {
             if (radioButtonNormal.Checked)
             {
